226ecb1857c3a55587721bc74a5325aadbbd1d1a
sema-demir <semademir0505@gmail.com>
2024-07-19 07:52:39 +0300

    custom-btn

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import CustomButton from "./CustomButton";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full absolute z-10">
+      <nav className=" max-w-[1140px] mx-auto flex  justify-between items-center sm:px-16 px-6 py-4">
+        <Link to={"/"}>
+          <img src="/bmw.png" alt="bmw-logo" width={50} />
+        </Link>
+        <CustomButton title="Kaydol" btnType="button" designs="min-w-[130px]" />
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/types/index.tsx
@@ -0,0 +1,6 @@
+//CustomButton bileşeninin aldığı propslar
+export type ButtonProps = {
+  title: string;
+  designs?: string;
+  btnType?: "button" | "submit";
+};
